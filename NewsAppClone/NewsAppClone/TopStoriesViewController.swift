14ec9b90e9ccd9daf439baa871abb58c90490682
yeonduing <yeonduing@gmail.com>
2023-07-25 00:26:15 +0900

    setup styling

--- NewsAppClone/NewsAppClone/TopStoriesViewController.swift
+++ NewsAppClone/NewsAppClone/TopStoriesViewController.swift
@@ -7,4 +7,27 @@
 
 import UIKit
 
-final class TopStoriesViewController: UIViewController { }
+final class TopStoriesViewController: UIViewController {
+
+  init() {
+    super.init(nibName: nil, bundle: nil)
+
+    setupStyle()
+    setupLayout()
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
+
+private extension TopStoriesViewController {
+
+  func setupStyle() {
+    view.backgroundColor = .background
+  }
+
+  func setupLayout() {
+
+  }
+}
