3cbdaf7c608826840d86d39be02dd5a975e15fdc
yeonduing <yeonduing@gmail.com>
2023-07-25 00:12:51 +0900

    add color assets

---
+++ NewsAppClone/NewsAppClone/UIColor+.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor+.swift
+//  NewsAppClone
+//
+//  Created by itzel.du on 2023/07/25.
+//
+
+import UIKit
+
+extension UIColor {
+
+  static let background: UIColor = .init(named: "background")!
+  static let cellBackground: UIColor = .init(named: "cellBackground")!
+
+  static let primary: UIColor = .init(named: "primary")!
+}
